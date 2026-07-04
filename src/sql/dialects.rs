17b86b61177c5503ad4b7a4a2e7ecc95d7d780b5
LugolBis <ld.desmares@gmail.com>
2026-07-04 11:04:19 +0200

    Add dialect build from string

---
+++ src/sql/dialects.rs
@@ -0,0 +1,22 @@
+use sqlparser::dialect::{
+    AnsiDialect, BigQueryDialect, Dialect, DuckDbDialect, GenericDialect, MsSqlDialect,
+    MySqlDialect, PostgreSqlDialect, SnowflakeDialect,
+};
+
+use crate::error::{DbtMigratorError, Result};
+
+/// Build the `sqlparser` dialect from the name.
+/// Use it to extend the functionalities and add the support of others dialect.
+pub fn dialect_from_name(name: &str) -> Result<Box<dyn Dialect>> {
+    match name.to_lowercase().as_str() {
+        "ansi" => Ok(Box::new(AnsiDialect {})),
+        "mssql" | "sqlserver" | "sql_server" | "tsql" => Ok(Box::new(MsSqlDialect {})),
+        "mysql" | "mariadb" => Ok(Box::new(MySqlDialect {})),
+        "postgres" | "postgresql" => Ok(Box::new(PostgreSqlDialect {})),
+        "duckdb" => Ok(Box::new(DuckDbDialect {})),
+        "snowflake" => Ok(Box::new(SnowflakeDialect {})),
+        "bigquery" => Ok(Box::new(BigQueryDialect {})),
+        "generic" => Ok(Box::new(GenericDialect {})),
+        other => Err(DbtMigratorError::UnknownDialect(other.to_string())),
+    }
+}
